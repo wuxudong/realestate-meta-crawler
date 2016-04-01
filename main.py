094244ba6f06768c5702919111ebd0c9472ef110
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-04-01 08:31:42 +0800

    修复无数据时，执行查询时出错的问题

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import leancloud
 from leancloud import Query
+from leancloud import LeanCloudError
 
 from check.check import *
 from check import check_crawler
@@ -12,11 +13,26 @@ leancloud.init('30WAnGl011OU88WAGBH5dmuz-gzGzoHsz', 'eBxQv0goJVso11f6zx6lbRJm')
 
 
 def main():
+
+    try:
+        query = Query(Check)
+        query.count()
+    except LeanCloudError as e:
+        empty = Check()
+        empty.save()
+
     for check in check_crawler.crawl_check():
         query = Query(Check)
         query.equal_to(CHECK_ID, check.get(CHECK_ID))
         if not query.find():
             check.save()
+
+    try:
+        query = Query(RegisterStatistics)
+        query.count()
+    except LeanCloudError as e:
+        empty = RegisterStatistics()
+        empty.save()
 
     for statistics in register_statistics_crawler.crawl_register_statistics():
         query = Query(RegisterStatistics)
